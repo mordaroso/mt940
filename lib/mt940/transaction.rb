77610858cfd8bcc516392e0fa3f22e7f6fc616ac
mordaroso <mordaroso@gmail.com>
2013-06-13 16:26:57 +0200

    Add type attribute to transaction

--- lib/mt940/transaction.rb
+++ lib/mt940/transaction.rb
@@ -2,7 +2,7 @@ module MT940
 
   class Transaction
 
-    attr_accessor :bank_account, :contra_account, :amount, :description, :contra_account_owner, :date, :bank, :currency
+    attr_accessor :bank_account, :contra_account, :amount, :description, :contra_account_owner, :date, :bank, :currency, :type
 
     def initialize(attributes = {})
       @bank_account        = attributes[:bank_account]
@@ -13,6 +13,7 @@ module MT940
       @contra_account      = attributes[:contra_account]
       @contra_account_name = attributes[:contra_account_owner]
       @currency            = attributes[:currency]
+      @type                = attributes[:type]
     end
 
   end
